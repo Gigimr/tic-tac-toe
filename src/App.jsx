dc45fe5fa0f89592239741b0278d4b10ad06d674
Gigi <gigi151521@gmail.com>
2024-04-29 15:37:19 -0600

    added localStorage functionality

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,17 @@ import Square from './components/Square.jsx'
 import {TURNS,WINNER_COMBOS} from './constants.js'
 
 function App() {
-const [board, setBoard] = useState(Array(9).fill(null))
+const [board, setBoard] = useState(()=> {
+  const boardFromStorage = window.localStorage.getItem('board')
+  return boardFromStorage ? JSON.parse(boardFromStorage) :  
+  Array(9).fill(null)
+})
 
-const [turn, setTurn]= useState(TURNS.X)
+const [turn, setTurn]= useState(()=>{
+  const turnFromStorage = window.localStorage.getItem('turn')
+  return turnFromStorage ?? TURNS.X
+})
+
 const [winner, setWinner] = useState(null)
 
 const checkWinner =(boardToCheck) =>{
@@ -25,6 +33,9 @@ const resetGame = () =>{
   setBoard(Array(9).fill(null))
   setTurn(TURNS.X)
   setWinner(null)
+
+  window.localStorage.removeItem('board')
+  window.localStorage.removeItem('turn')
 }
 
 const checkEndGame =(newBoard)=>{
@@ -40,6 +51,8 @@ const updateBoard = (index) => {
 
   const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
   setTurn(newTurn)
+  window.localStorage.setItem('board', JSON.stringify(newBoard))
+  window.localStorage.setItem('turn', newTurn)
   const  newWinner = checkWinner(newBoard)
   if(newWinner){
   confetti()
